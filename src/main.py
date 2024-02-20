880dccd2be76b404be65fc6dd29816e080d720ce
Wesllen Sousa Lima <wesllen.wsl@gmail.com>
2024-02-20 08:24:37 -0300

    feat: added data resources in package

--- src/main.py
+++ src/main.py
@@ -1,14 +1,20 @@
 
 import matplotlib.pyplot as plt
 
+#import wpycounts
+#counts = wpycounts.count_words("zen.txt")
+#print(counts)
+
 from wpycounts.wpycounts import count_words
 from wpycounts.plotting import plot_words
+from wpycounts.datasets import get_flatland
 
 # Inline documentation
 # help(plot_words)
 
-counts = count_words("zen.txt")
+flatland = get_flatland()
+counts = count_words(flatland)
 print(counts)
 
 fig = plot_words(counts, 10)
-plt.show()
+# plt.show()
--- src/wpycounts/__init__.py
+++ src/wpycounts/__init__.py
@@ -1,3 +1,7 @@
 # read version from installed package
 from importlib.metadata import version
 __version__ = version("wpycounts")
+
+# populate package namespace
+from wpycounts.wpycounts import count_words
+from wpycounts.plotting import plot_words
